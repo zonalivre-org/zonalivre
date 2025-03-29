6fba94e27c461290605fff943f9a7ab7b79ce497
Eric-Catarina <orment.cp@gmail.com>
2025-03-29 15:51:02 -0300

    feat: dog

---
+++ Assets/Navigation/DogMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DogMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    public List<Transform> destinationTransform;
+    private Transform currentDestination;
+    private bool isMoving = false;
+    [SerializeField]
+    private float timeBetweenMove = 2f;
+    
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+    void Start()
+    {
+        MoveToDestination();
+    }
+
+  
+    void MoveToDestination()
+    {
+        currentDestination = destinationTransform[Random.Range(0, destinationTransform.Count)];
+        agent.SetDestination(currentDestination.position);
+        Invoke("MoveToDestination", timeBetweenMove);
+    }
+    
+}
