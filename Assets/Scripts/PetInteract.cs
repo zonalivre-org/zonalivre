bdfe8546c6e60370011fa562f6fa055be6137773
Gabriel Logan Dias Porto <gabriellogand@gmail.com>
2025-04-28 10:21:12 -0300

    fix: dog not stoping on click

--- Assets/Scripts/PetInteract.cs
+++ Assets/Scripts/PetInteract.cs
@@ -17,10 +17,11 @@ public class PetInteract : MonoBehaviour
     [SerializeField] private GameObject staminaMinigameUI;
     [SerializeField] private GameObject happynessMinigameUI;
     private InGameProgress inGameProgress;
-    private bool enable = false, interactable = true;
+    private bool enableMinigameStart = false, interactable = true;
     private int defineObjective = 2;
     [Header("Place Holder Variables for Debugging Porpuses")]
     [SerializeField] private LayerMask healthLayer, staminaLayer, happynessLayer;
+    private PlayerInventory playerInventory;
     private void Awake() => inGameProgress = FindObjectOfType<InGameProgress>();
     private void LateUpdate()
     {
@@ -28,13 +29,27 @@ public class PetInteract : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if(enable && other.gameObject.CompareTag("Player"))
+        if(enableMinigameStart && other.gameObject.CompareTag("Player"))
         {
             dogMovement.canAutoMove = false;
             dogMovement.SetAutonomousMovement(false);
             Debug.Log("Entrou na area do pet!");
             Invoke ("StartMinigame", detectionDelay);
         }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (enableMinigameStart && other.gameObject.CompareTag("Player"))
+        {
+            dogMovement.canAutoMove = false;
+            dogMovement.SetAutonomousMovement(false);
+            Invoke(nameof(StartMinigame), detectionDelay);
+
+
+        }
+
+
     }
 
     private void OnTriggerExit(Collider other)
@@ -51,32 +66,17 @@ public class PetInteract : MonoBehaviour
         RaycastHit hit;
 
         //Placehoulder code bellow VVV
-        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, healthLayer)) 
-        {
-            InventoryManager.instance.Search(101, true); 
-            Debug.Log(InventoryManager.instance.currentItem.name);
-        }
-        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, staminaLayer))
-        {
-            InventoryManager.instance.Search(102, true); 
-            Debug.Log(InventoryManager.instance.currentItem.name);
-        }
-        if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, happynessLayer)) 
-        {
-            InventoryManager.instance.Search(103, true); 
-            Debug.Log(InventoryManager.instance.currentItem.name);
-        }
         //Placehoulder code above ^^^
 
         if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity, clicklableLayers))
         {
-            enable = true;
-            dogMovement.SetAutonomousMovement(true);
+            enableMinigameStart = true;
+            dogMovement.SetAutonomousMovement(false);
             Debug.Log("Indo olhar o pet!");
         }
-        else if(enable)
+        else if(enableMinigameStart)
         {
-            enable = false;
+            enableMinigameStart = false;
             dogMovement.SetAutonomousMovement(true);
             Debug.Log("cancelou a ação");
         }
@@ -100,7 +100,6 @@ public class PetInteract : MonoBehaviour
         }
         else Debug.Log(whichTask + " is not a valid Pet task number!");
 
-        InventoryManager.instance.Search(0, true);
         playerMovement.ToggleMovement(true);
         dogMovement.SetAutonomousMovement(true);
         dogMovement.canAutoMove = true;
@@ -111,31 +110,26 @@ public class PetInteract : MonoBehaviour
     {
         playerMovement.ToggleMovement(true);
         dogMovement.SetAutonomousMovement(true);
-        enable = false;
+        enableMinigameStart = false;
         interactable = true;
         Debug.Log("Cancelou a ação");
     }
 
     private void StartMinigame() // receives a value from within this script between 0 and 2 then activate the minigame corresponding to whatever pet slider value is to be changed.
     {
-        if(enable)
+        if(enableMinigameStart)
         {
             playerMovement.ToggleMovement(false);
-            InventoryManager imi = InventoryManager.instance;
-            if(imi.currentItem.id == 101) //Minigame de curar a vida
-            {
+          
                 healthMinigameUI.SetActive(true);
-            }
-            else if(imi.currentItem.id == 102) //Minigame de alimentar
-            {
+            
+            
                 staminaMinigameUI.SetActive(true);
-            }
-            else if(imi.currentItem.id == 103 || imi.currentItem.id == 0) //Minigame de brincar
-            {
+           
+           
                 happynessMinigameUI.SetActive(true);
-            }
-            else Debug.Log(imi.currentItem.id + " is not a valid ID number!");
-            enable = false;
+            
+            enableMinigameStart = false;
             interactable = false;
         }
     }
